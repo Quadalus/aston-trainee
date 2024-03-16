d6f6c5b805772a84109193e3af0e466e5063be14
quadalus <quadalus@yandex.ru>
2024-03-16 04:06:45 +0300

    feat: сделал реализацию CustomArrayList

---
+++ src/main/java/hw1/ArrayUtil.java
@@ -0,0 +1,51 @@
+package hw1;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class ArrayUtil {
+    @SuppressWarnings({"rawtypes", "unchecked"})
+    public static Object[] mergeSort(Object[] array, Comparator comparator) {
+        if (array.length <= 1) {
+            return array;
+        }
+
+        if (comparator == null) {
+            comparator = Comparator.reverseOrder();
+        }
+
+        Object[] result = new Object[array.length];
+        Object[] left = mergeSort(Arrays.copyOfRange(array, 0, array.length / 2), comparator);
+        Object[] right = mergeSort(Arrays.copyOfRange(array, array.length / 2, array.length), comparator);
+        int leftIndex = 0;
+        int rightIndex = 0;
+        int resultIndex = 0;
+
+        while (leftIndex < left.length && rightIndex < right.length) {
+            var leftObject = left[leftIndex];
+            var rightObject = right[rightIndex];
+
+            if (comparator.compare(leftObject, rightObject) <= 0) {
+                result[resultIndex] = leftObject;
+                leftIndex++;
+            } else {
+                result[resultIndex] = rightObject;
+                rightIndex++;
+            }
+            resultIndex++;
+        }
+
+        while (leftIndex < left.length) {
+            result[resultIndex] = left[leftIndex];
+            leftIndex++;
+            resultIndex++;
+        }
+
+        while (rightIndex < right.length) {
+            result[resultIndex] = right[rightIndex];
+            rightIndex++;
+            resultIndex++;
+        }
+        return result;
+    }
+}
