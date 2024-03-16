fa23acae2a2bb7ab87d340d7700586f20688188f
quadalus <quadalus@yandex.ru>
2024-03-16 04:07:50 +0300

    feat: покрыл тестами

--- src/test/java/hw1/CustomArrayTest.java
+++ src/test/java/hw1/CustomArrayTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashSet;
 import java.util.Set;
