368b16d5db84fd25869cb69f8bdc0d41c5aca085
quadalus <quadalus@yandex.ru>
2024-03-22 12:56:01 +0300

    feat: добавил UserDto

---
+++ src/main/java/hw2/dto/UserDto.java
@@ -0,0 +1,53 @@
+package hw2.dto;
+
+import java.util.Objects;
+
+public class UserDto {
+    private String username;
+    private String email;
+
+    public UserDto(String username, String email) {
+        this.username = username;
+        this.email = email;
+    }
+
+    public UserDto() {
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        UserDto userDto = (UserDto) object;
+        return Objects.equals(username, userDto.username) && Objects.equals(email, userDto.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(username, email);
+    }
+
+    @Override
+    public String toString() {
+        return "UserDto{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
